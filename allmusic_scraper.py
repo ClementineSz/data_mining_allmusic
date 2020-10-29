a2ccc1a131a8bdda7a150b1fc09519df9a3eca00
ClementineSzalavecz <clementine.szalavecz@gmail.com>
2020-10-29 11:51:32 +0100

    debut dic

--- allmusic_scraper.py
+++ allmusic_scraper.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 NEW_RELEASES = 'https://www.allmusic.com/newreleases'
 
@@ -13,4 +14,17 @@ def get_new_releases():
     ]
     """
     response = requests.get(NEW_RELEASES)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    features = {}
+    content_dif = soup.find_all('div', {"class" : "meta-container"})
+
+    print(content_dif)
+
+    # for div in content_dif:
+    #
+    #
+    # print(features)
+
     # TODO
