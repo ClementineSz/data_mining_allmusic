b1b512f9edc5368accab4356ae79b0c5995519ca
ClementineSzalavecz <clementine.szalavecz@gmail.com>
2020-11-06 13:02:11 +0100

    correction id

--- models/album_details.py
+++ models/album_details.py
@@ -4,9 +4,9 @@ from models.soup_loader import SoupLoader
 
 
 class AlbumDetails(SoupLoader):
-    def __init__(self, url, id):
+    def __init__(self, url):
         super().__init__(url=url)
-        self.id =
+
 
     @property
     def duration(self):
