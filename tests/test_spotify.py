70922fa0252e351e4fd8d69c2a8819e409647bf0
ClementineSzalavecz <clementine.szalavecz@gmail.com>
2020-12-09 19:40:36 +0100

    feat : get popularity and followers of artist

--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -18,11 +18,20 @@ def test_get_client_secret():
 def test_get_access_token():
     album_id = SpotifyApi.get_album_id('Moment', 'Dark Tranquillity')
     print(album_id)
-    album_info = SpotifyApi.get_album_info(album_id)
+
+
+# album_info = SpotifyApi.get_album_info(album_id)
 # def test_get_album_popularity(spotify_album_id):
 
 
 def test_get_album_info():
-    #album_id = SpotifyApi.get_album_id('Moment', 'Dark Tranquillity')
-    album_info = SpotifyApi.get_album_info('Moment', 'Dark Tranquillity')
+    # album_id = SpotifyApi.get_album_id('Moment', 'Dark Tranquillity')
+    album_info = SpotifyApi.get_album_popularity('Moment', 'Dark Tranquillity')
     print(album_info)
+
+
+def test_get_artist_info():
+    #artist_id = SpotifyApi.get_artist_id('Moment', 'Dark Tranquillity')
+    artist_popularity = SpotifyApi.get_artist_popularity('Moment', 'Dark Tranquillity')
+    artist_followers = SpotifyApi.get_artist_followers('Moment', 'Dark Tranquillity')
+    print(artist_popularity,artist_followers)
