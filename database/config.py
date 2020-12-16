31681d4da78743c65ab04f0b71dc0643f2be571d
ClementineSzalavecz <clementine.szalavecz@gmail.com>
2020-12-16 23:19:14 +0100

    fix

--- database/config.py
+++ database/config.py
@@ -1,3 +1,3 @@
 
 DB_NAME = 'db_albums'
-SQL_URL = f'mysql+mysqlconnector://alexclem:alexclem1990@ec2-54-177-174-191.us-west-1.compute.amazonaws.com/:3306'
+SQL_URL = f'mysql+mysqlconnector://alexclem:alexclem1990@localhost'
