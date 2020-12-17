a48c83972e9ca6c43a3edad445cd3cce9a0cb078
Alexandre de Pape <alexandredepape@gmail.com>
2020-12-17 15:20:29 +0200

    fix: sql connection string

--- database/config.py
+++ database/config.py
@@ -1,3 +1,8 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 DB_NAME = 'db_albums'
-SQL_URL = 'mysql://root:alex1995@localhost/'
-# SQL_URL = f'mysql+mysqlconnector://root:alex1995@localhost/'
+SQL_URL = f'mysql+mysqldb://{os.environ["SQL_USER"]}:{os.environ["SQL_PASSWORD"]}@localhost/'
