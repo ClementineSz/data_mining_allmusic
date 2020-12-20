49150c48b1a3f6b59a49b116aa6495b4ec2a20d7
Alexandre de Pape <alexandredepape@gmail.com>
2020-12-20 14:10:33 +0200

    fix: @protected_from_attribute_error in headline

--- scraping/headline.py
+++ scraping/headline.py
@@ -1,10 +1,12 @@
 from scraping.config import HtmlTags, HtmlClasses
+from scraping.utils import protected_from_attribute_error
 
 
 class Headline:
     def __init__(self, soup):
         self.soup = soup
 
+    @protected_from_attribute_error
     @property
     def author(self):
         """ Extracts author of headline from the html
@@ -13,6 +15,7 @@ class Headline:
         """
         return self.soup.find(HtmlTags.DIV, {'class': HtmlClasses.AUTHOR}).text.strip('\n -')
 
+    @protected_from_attribute_error
     @property
     def content(self):
         """ Extracts content of headline from the html
